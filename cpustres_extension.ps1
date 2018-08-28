373dcd3a2dbf914a0ff47c16bf43273bab322479
Dale Marshall <dale.marshall@ansgroup.co.uk>
2018-08-28 12:57:34 +0100

    first commit

---
+++ cpustres_extension.ps1
@@ -0,0 +1,17 @@
+﻿# download cpustres.zip
+New-Item -Path "c:\" -Name "bootcamp" -ItemType "directory"
+Invoke-WebRequest -Uri http://download.sysinternals.com/files/CPUSTRES.zip -OutFile "C:\bootcamp\cpustres.zip"
+
+
+#unzip cpustres.zip
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip "C:\bootcamp\cpustres.zip" "C:\bootcamp"
+
+#Start cpustres.exe
